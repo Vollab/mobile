cf9a8c809f24dafbeab540f375b7aa174976b1f9
Miguel <miguelandradebarreto2@gmail.com>
2023-02-10 10:16:56 -0300

    :construction: update useResponsiveScale

--- App.tsx
+++ App.tsx
@@ -1,12 +1,7 @@
 import '@src/styles'
 
-import AuthSelect from '@src/screens/Auth/AuthSelect'
-import CandidateSignUp from '@src/screens/Auth/CandidateSignUp'
-import RequesterSignUp from '@src/screens/Auth/RequesterSignUp'
-import SignIn from '@src/screens/Auth/SignIn'
-import SignUp from '@src/screens/Auth/SignUp'
+import Test from '@src/screens/Test'
 
-import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import type { NativeStackScreenProps } from '@react-navigation/native-stack'
 
@@ -26,17 +21,18 @@ export type TRootStackScreen<T extends keyof TRootStackParamList> =
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Navigator
-        initialRouteName='AuthSelect'
-        screenOptions={{ headerShown: false, animation: 'fade_from_bottom' }}
-      >
-        <Screen name='AuthSelect' component={AuthSelect} />
-        <Screen name='SignIn' component={SignIn} />
-        <Screen name='SignUp' component={SignUp} />
-        <Screen name='RequesterSignUp' component={RequesterSignUp} />
-        <Screen name='CandidateSignUp' component={CandidateSignUp} />
-      </Navigator>
-    </NavigationContainer>
+    // <NavigationContainer>
+    //   <Navigator
+    //     initialRouteName='AuthSelect'
+    //     screenOptions={{ headerShown: false, animation: 'fade_from_bottom' }}
+    //   >
+    //     <Screen name='AuthSelect' component={AuthSelect} />
+    //     <Screen name='SignIn' component={SignIn} />
+    //     <Screen name='SignUp' component={SignUp} />
+    //     <Screen name='RequesterSignUp' component={RequesterSignUp} />
+    //     <Screen name='CandidateSignUp' component={CandidateSignUp} />
+    //   </Navigator>
+    // </NavigationContainer>
+    <Test />
   )
 }
--- src/hooks/useResponsiveScale.ts
+++ src/hooks/useResponsiveScale.ts
@@ -3,79 +3,105 @@ import { useWindowDimensions } from 'react-native'
 import { pxToNumber } from '@src/utils/px'
 
 interface IResponsiveScaleParams {
-  size: string
+  strength?: number
+  size: string | number
   limits?: { max?: number; min?: number }
   type?: 'moderate' | 'vertical' | 'horizontal'
 }
 
 type TResponsiveScale = (params?: IResponsiveScaleParams) => number
 
-type TVerifyLimits = (params: {
-  min: number
-  max: number
-  size: number | string
-  scaledSize: number
-}) => number
-
-type TScale = (params: {
-  min?: number
-  max?: number
-  size: number | string
-}) => number
-
-const verifyLimits: TVerifyLimits = ({ size, max, min, scaledSize }) => {
-  let newSize = scaledSize
-  const maxValue = pxToNumber(size) * max
-  const minValue = pxToNumber(size) * min
-
-  if (min) newSize = newSize <= minValue ? minValue : newSize
-  if (max) newSize = newSize >= maxValue ? maxValue : newSize
-
-  return newSize
-}
-
 const useResponsiveScale = () => {
-  const factor = 0.5
-  const guidelineBaseWidth = 400
-  const guidelineBaseHeight = 800
-
   const { width, height } = useWindowDimensions()
+
+  let guidelineBaseWidth = 400
+  let guidelineBaseHeight = 800
+  const isPortrait = width <= height
+
+  if (!isPortrait) {
+    const aux = guidelineBaseHeight
+    guidelineBaseHeight = guidelineBaseWidth
+    guidelineBaseWidth = aux
+  }
 
   const responsiveScale: TResponsiveScale = ({
     size,
     limits,
+    strength = 0.5,
     type = 'moderate'
   }) => {
     let newSize = pxToNumber(size)
+    const verticalProportion = height / guidelineBaseHeight
+    const horizontalProportion = width / guidelineBaseWidth
+    const bothProportion = (verticalProportion + horizontalProportion) / 2
 
-    const vertical = (height / guidelineBaseHeight) * newSize
-    const horizontal = (width / guidelineBaseWidth) * newSize
+    const applyStrength = (scaledSize: number) =>
+      newSize + (scaledSize - newSize) * (strength * 2)
 
     switch (type) {
       case 'horizontal':
-        newSize = horizontal
+        newSize = applyStrength(horizontalProportion * newSize)
         break
 
       case 'vertical':
-        newSize = vertical
+        newSize = applyStrength(verticalProportion * newSize)
         break
 
       default: {
-        const verticalProportion = vertical - newSize
-        const horizontalProportion = horizontal - newSize
-        const bothProportion = verticalProportion + horizontalProportion
-
-        newSize = newSize + bothProportion * factor
+        newSize = applyStrength(bothProportion * newSize)
         break
       }
+    }
+
+    if (limits) {
+      const maxSize = limits.max * pxToNumber(size)
+      const minSize = limits.min * pxToNumber(size)
+
+      newSize = newSize >= maxSize ? maxSize : newSize
+      newSize = newSize <= minSize ? minSize : newSize
     }
 
     return newSize
   }
 
-  return { responsiveScale, width, height }
+  return { responsiveScale, screenWidth: width, screenHeight: height }
 }
 
 export default useResponsiveScale
 
-// https://medium.com/soluto-engineering/size-matters-5aeeb462900a
+/*
+  Inspiration:
+    Soluto - Size Matters
+    https://medium.com/soluto-engineering/size-matters-5aeeb462900a
+
+  if type = vertical the scale only apply in vertical
+  if type = horizontal the scale only apply in horizontal
+  if type = moderate the scale apply in average of vertical and horizontal
+
+  Example:
+    Strength = 0.5
+    Base = 400x800
+
+  Object size
+    in 400x800 = 100x100
+    in 200x800 = 75x75
+    in 400x400 = 75x75
+    in 200x400 = 50x50
+
+  Horizontal size is more shocking because: baseWidth < baseHeight
+
+  Strength determines how much will be scaled (in 400x800 = 100x100):
+  All sizes in 200x400 only changing the strength
+
+  Strength = 0.25
+  75x75
+
+  Strength = 0.5
+  50x50
+
+  Strength = 0.75
+  25x25
+
+  Strength = 1
+  0x0
+*/
